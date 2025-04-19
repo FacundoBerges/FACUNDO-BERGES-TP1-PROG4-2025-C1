14ab8ea1213834a5c06e3be608ed99d77d4bf096
Facundo Berges <facundo.h.berges@gmail.com>
2025-04-19 18:45:36 -0300

    sección información de usuario github

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -1,14 +1,16 @@
 import { Component, input, InputSignal } from '@angular/core';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, NgOptimizedImage } from '@angular/common';
 
 import { GithubUser } from '../../interfaces/github-user';
 
 @Component({
   selector: 'juegos-info',
-  imports: [JsonPipe],
+  imports: [JsonPipe, NgOptimizedImage],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss',
 })
 export class InfoComponent {
-  public user: InputSignal<GithubUser | undefined> = input<GithubUser | undefined>();
+  public user: InputSignal<GithubUser | undefined> = input<
+    GithubUser | undefined
+  >();
 }
