2adbb3ac29b2372b3bc18f49b443d3029b2f909f
Facundo Berges <facundo.h.berges@gmail.com>
2025-04-19 19:15:52 -0300

    componente de carga

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -2,10 +2,11 @@ import { Component, input, InputSignal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 
 import { GithubUser } from '../../interfaces/github-user';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
   selector: 'juegos-info',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, LoaderComponent],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss',
 })
