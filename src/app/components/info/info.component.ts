b15a83283592422b5c330fe0e495e5f5f847b283
Facundo Berges <facundo.h.berges@gmail.com>
2025-04-18 23:37:32 -0300

    comienzo de componente sobre mí

---
+++ src/app/components/info/info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'juegos-info',
+  imports: [],
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.scss'
+})
+export class InfoComponent {
+
+}
