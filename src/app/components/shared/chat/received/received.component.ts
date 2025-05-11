ef36701e49f928eef4e38ecc7660b8629075393c
Facundo Berges <facundo.h.berges@gmail.com>
2025-05-11 14:15:54 -0300

    funcionalidad chat con mensajes a tiempo real

--- src/app/components/shared/chat/received/received.component.ts
+++ src/app/components/shared/chat/received/received.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, InputSignal } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 import { Message } from '../../../../interfaces/message';
 
 @Component({
   selector: 'juegos-received',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './received.component.html',
   styleUrl: './received.component.scss',
 })
--- src/app/components/shared/chat/sended/sended.component.ts
+++ src/app/components/shared/chat/sended/sended.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, InputSignal } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 import { Message } from '../../../../interfaces/message';
 
 @Component({
   selector: 'juegos-sended',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './sended.component.html',
   styleUrl: './sended.component.scss',
 })
